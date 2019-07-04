93f06c2718b9e50adeab5dac9686ef9de67efff6
ClaudioAldrighetti <aldrighetticlaudio98@gmail.com>
2019-07-04 11:29:29 +0200

    Implemented classes Administration and Prescription

--- src/it/univr/terapiaintensiva/model/Administration.java
+++ src/it/univr/terapiaintensiva/model/Administration.java
@@ -1,4 +1,42 @@
 package it.univr.terapiaintensiva.model;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Administration {
+    private final LocalDate date;
+    private final LocalTime time;
+    private final String medicine;
+    private final float dose;
+    private final String notes;
+
+    public Administration(LocalDate date, LocalTime time, String medicine, float dose, String notes){
+        this.date = date;
+        this.time = time;
+        this.medicine = medicine;
+        this.dose = dose;
+        this.notes = notes;
+    }
+
+    public Administration(LocalDate date, LocalTime time, String medicine, float dose){
+//        this.Administration(date, time, medicine, dose, "");
+        this.date = date;
+        this.time = time;
+        this.medicine = medicine;
+        this.dose = dose;
+        this.notes = "";
+    }
+
+    public String toString() {
+        String timeStr = time.getHour() + ":" + time.getMinute();
+        String noteStr = ( notes.equals("") ? "" : notes + "\n");
+        return
+                (
+                 "\nDate of administration: " + date +
+                 "\nTime of administration: " + timeStr +
+                 "\nMedicine: " + medicine +
+                 "\nDose: " + dose +
+                 "\n" + noteStr
+                );
+    }
 }
--- src/it/univr/terapiaintensiva/model/Prescription.java
+++ src/it/univr/terapiaintensiva/model/Prescription.java
@@ -1,4 +1,31 @@
 package it.univr.terapiaintensiva.model;
 
+import java.time.LocalDate;
+
 public class Prescription {
+    private final LocalDate date;
+    private final int duration;
+    private final String medicine;
+    private final int nDoses;
+    private final float dose;
+
+    public Prescription(LocalDate date, int duration, String medicine, int nDoses, float dose){
+        this.date = date;
+        this.duration = duration;
+        this.medicine = medicine;
+        this.nDoses = nDoses;
+        this.dose = dose;
+    }
+
+    public String toString() {
+        return
+               (
+                "\nDate of prescription: " + date +
+                "\nDuration of prescription: " + duration +
+                "\nMedicine: " + medicine +
+                "\nDaily doses: " + nDoses +
+                "\nQuantity of medicine for each dose: " + dose +
+                "\n"
+               );
+    }
 }
