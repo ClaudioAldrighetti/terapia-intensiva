750b22fc8f121081713fdb49215a82f6e66036d8
ClaudioAldrighetti <aldrighetticlaudio98@gmail.com>
2019-07-05 12:02:48 +0200

    Added ausiliar functions for csv files in Model Modified UC1

--- src/it/univr/terapiaintensiva/model/Model.java
+++ src/it/univr/terapiaintensiva/model/Model.java
@@ -5,53 +5,47 @@ import java.io.*;
 public class Model {
 
     private FileInputStream autenticationFile;
+    private String pathAutenticationFile;
 
     // Constructor
-    public Model() throws IOException {
-        try {
-            this.autenticationFile = new FileInputStream("../../assets/login.csv");
-        }
-        finally {
-            if(autenticationFile != null)
-                autenticationFile.close();
-        }
+    public Model() {
+        autenticationFile = null;
+        pathAutenticationFile = "../../assets/login.csv";
         //TODO
     }
 
     // UC1
     public boolean autenticate(String username, String password) throws IOException {
-        while(true) {
-            // Check if there is an user in this line
-            int checklLine = autenticationFile.read();
-            if (checklLine == -1)
-                return false;
+        try {
+            autenticationFile = new FileInputStream(pathAutenticationFile);
+            while(true) {
+                // Check if it is possible to read an entry from autenticationFile
+                if(csvEmptyFileInputStream(autenticationFile)) {
+                    autenticationFile.close();
+                    return false;
+                }
 
-            // Get username from File
-            String usr = "";
-            char c = (char) checklLine;
-            do {
-                usr += c;
-                c = (char) autenticationFile.read();
-            } while (c != ',');
+                // Get username from File
+                String usr = csvReadStringValue(autenticationFile);
 
-            // Username found
-            if (usr.equals(username)) {
-                // Check password
-                c = (char) autenticationFile.read();
-                for (int i = 0; i < password.length(); i++)
+                // User found
+                if (usr.equals(username)) {
+                    // Get user's password
+                    autenticationFile = csvNextValue(autenticationFile);
+                    String pswd = csvReadStringValue(autenticationFile);
+                    autenticationFile.close();
 
-                    // Invalid password
-                    if (c != password.charAt(i))
-                        return false;
+                    // Check password
+                    return pswd.equals(password);
+                }
 
-                // User verified
-                return true;
+                // Next line
+                autenticationFile = csvNextLine(autenticationFile);
             }
 
-            // Next line
-            do {
-                c = (char) autenticationFile.read();
-            } while (c != '\n');
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return false;
         }
     }
 
@@ -83,5 +77,41 @@ public class Model {
     // UC12
     public void changeVitals(String cf, Vitals vitals){
         //TODO
+    }
+
+    // AUSILIAR CSV FILE FUNCTIONS
+
+    // Checks if csv file has something to read
+    private boolean csvEmptyFileInputStream(FileInputStream readingFile) throws IOException {
+        return (readingFile == null || readingFile.read() == -1);
+    }
+
+    // Reads a value from csv file and returns it as a String
+    private String csvReadStringValue(FileInputStream readingFile) throws IOException {
+        if(csvEmptyFileInputStream(readingFile))
+            return null;
+
+        String valueStr = "";
+        for(char c=(char)readingFile.read(); c!=',' && c!='\n' ;c=(char)readingFile.read())
+            valueStr += c;
+        return valueStr;
+    }
+
+    // Goes to next value in this csv file
+    private FileInputStream csvNextValue(FileInputStream readingFile) throws IOException {
+        if(csvEmptyFileInputStream(readingFile))
+            return null;
+
+        for(char c = (char)readingFile.read(); c!=',' && c!='\n'; c = (char)readingFile.read());
+        return readingFile;
+    }
+
+    // Goes to next line in this csv file
+    private FileInputStream csvNextLine(FileInputStream readingFile) throws IOException {
+        if(csvEmptyFileInputStream(readingFile))
+            return null;
+
+        for(char c = (char)readingFile.read(); c!='\n'; c = (char)readingFile.read());
+        return readingFile;
     }
 }
