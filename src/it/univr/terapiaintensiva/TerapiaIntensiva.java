6d4e8a45a9525ade370518b5b5f633e59593906d
ClaudioAldrighetti <aldrighetticlaudio98@gmail.com>
2019-07-13 15:30:20 +0200

    TerapiaIntensiva: Aggiunto metodo main()

--- src/it/univr/terapiaintensiva/TerapiaIntensiva.java
+++ src/it/univr/terapiaintensiva/TerapiaIntensiva.java
@@ -3,7 +3,11 @@ import it.univr.terapiaintensiva.model.*;
 import it.univr.terapiaintensiva.view.*;
 
 public class TerapiaIntensiva {
-    public static void Main(String[] args){
+    public static void main(String[] args){
+        // Memory loading and configuration
         Model model = new Model();
+
+        // First frame
+        LoginFrame loginFrame = new LoginFrame();
     }
 }
