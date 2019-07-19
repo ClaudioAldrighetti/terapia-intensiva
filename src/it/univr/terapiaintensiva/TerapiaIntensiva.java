a58a3a458c25989f343896a1761e31e1028c642d
ecavicc <enrico.cavicchini@gmail.com>
2019-07-19 13:35:42 +0200

    Modificato commento

--- src/it/univr/terapiaintensiva/TerapiaIntensiva.java
+++ src/it/univr/terapiaintensiva/TerapiaIntensiva.java
@@ -8,7 +8,7 @@ import javax.swing.*;
 public class TerapiaIntensiva {
     public static void main(String[] args) {
         try {
-            // Set cross-platform Java L&F (also called "Metal")
+            // Set Nimbus LaF
             UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
         } catch (Exception e) {
             e.printStackTrace();
