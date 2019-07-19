f83fcf9ec199b66338929d845c8814211a1e16ec
ecavicc <enrico.cavicchini@gmail.com>
2019-07-19 13:05:47 +0200

    Aggiunto LaF Nimbus

--- src/it/univr/terapiaintensiva/TerapiaIntensiva.java
+++ src/it/univr/terapiaintensiva/TerapiaIntensiva.java
@@ -3,8 +3,16 @@ package it.univr.terapiaintensiva;
 import it.univr.terapiaintensiva.model.Model;
 import it.univr.terapiaintensiva.view.LoginFrame;
 
+import javax.swing.*;
+
 public class TerapiaIntensiva {
     public static void main(String[] args) {
+        try {
+            // Set cross-platform Java L&F (also called "Metal")
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         Model model = Model.getIstance();
         LoginFrame loginFrame = new LoginFrame();
     }
