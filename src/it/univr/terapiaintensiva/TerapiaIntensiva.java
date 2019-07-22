7127a004f798311dd942a90df99d70e623811655
ecavicc <enrico.cavicchini@gmail.com>
2019-07-22 18:07:29 +0200

    Aggiunta build del jar e rimosso Nimbus LaF

--- src/it/univr/terapiaintensiva/TerapiaIntensiva.java
+++ src/it/univr/terapiaintensiva/TerapiaIntensiva.java
@@ -3,16 +3,14 @@ package it.univr.terapiaintensiva;
 import it.univr.terapiaintensiva.model.Model;
 import it.univr.terapiaintensiva.view.LoginFrame;
 
-import javax.swing.*;
-
 public class TerapiaIntensiva {
     public static void main(String[] args) {
-        try {
-            // Set Nimbus LaF
-            UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+//        try {
+//            // Set Nimbus LaF
+//            UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
         Model model = Model.getInstance();
         LoginFrame loginFrame = new LoginFrame();
     }
